3c4c69c47b9cd66c6989f565df886bc721f10407
Namless-Programm <zelos.games69@gmail.com>
2026-07-07 14:53:00 +0200

    Create FocusTrackerApplication.java

---
+++ src/main/java/com/namlessprogramm/focustracker/FocusTrackerApplication.java
@@ -0,0 +1,11 @@
+package com.namlessprogramm.focustracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusTrackerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FocusTrackerApplication.class, args);
+    }
+}
