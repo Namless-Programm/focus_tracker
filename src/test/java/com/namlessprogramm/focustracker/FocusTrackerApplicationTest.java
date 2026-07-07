ffc45d257fb0cdcad8214939658f9f00164fc99d
Namless-Programm <zelos.games69@gmail.com>
2026-07-07 14:53:35 +0200

    Create FocusTrackerApplicationTest.java

---
+++ src/test/java/com/namlessprogramm/focustracker/FocusTrackerApplicationTest.java
@@ -0,0 +1,12 @@
+package com.namlessprogramm.focustracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocusTrackerApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
